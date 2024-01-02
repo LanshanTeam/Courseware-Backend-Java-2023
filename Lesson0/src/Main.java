c2174e6231db166d7384cc03f7276ff350f38431
dls2233 <116965982+dls2233@users.noreply.github.com>
2024-01-02 13:00:12 +0800

    lesson7

---
+++ Lesson0/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author 黄一峰
+ * @date 2023/10/23 16:07
+ * @description ${NAME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
